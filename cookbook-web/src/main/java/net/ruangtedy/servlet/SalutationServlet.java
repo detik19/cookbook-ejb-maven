87d77e3fd3e359ad9fec32d9da9afde8671be7c9
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2016-08-28 14:00:40 +0700

    call using jndi

--- cookbook-web/src/main/java/net/ruangtedy/servlet/SalutationServlet.java
+++ cookbook-web/src/main/java/net/ruangtedy/servlet/SalutationServlet.java
@@ -3,6 +3,9 @@ package net.ruangtedy.servlet;
 import java.io.IOException;
 
 import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,11 +17,23 @@ import net.ruangtedy.ejb.Salutation;
 @WebServlet(value="/salutation", name="salutation-test")
 public class SalutationServlet extends HttpServlet{
 
-	@EJB
+
 	private Salutation salutation;
 
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Context context=null;
+		try {
+			context=new InitialContext();
+			salutation=(Salutation) context.lookup("java:global/cookbookapp/cookbook-services-0.0.1-SNAPSHOT/Salutation!net.ruangtedy.ejb.Salutation");
+		
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//salutation=context.lookup(name)
+				
 		String hy=salutation.getFormalSalutation("Tedy");
 		resp.getWriter().println(hy);
 		
